9ec6df0d8cbe90961e77fdf938267769f966ee13
DARDENNE Gregory <gdardenne0@gmail.com>
2022-04-06 15:46:57 +0200

    Ajout du test activerLampe position 0

--- Domotique/test/TelecommandeTest.java
+++ Domotique/test/TelecommandeTest.java
@@ -12,7 +12,17 @@ class TelecommandeTest {
     }
 
     @Test
-    void activerLampe() {
+    void activerLampe_pos0() {
+        // Initialisation des valeurs
+        Telecommande t = new Telecommande();
+        Lampe p = new Lampe("Lampe1");
+        t.ajouterLampe(p);
+
+        // Appel de la methode
+        t.activerLampe(0);
+
+        // Test si la valeur a bien ete changer
+        assertTrue(p.isAllume(), "La lampe ne s'est pas allumee");
     }
 
     @Test

ed1301c78655ec90e1a6075ab0265abd66db3e19
DARDENNE Gregory <gdardenne0@gmail.com>
2022-04-06 14:57:02 +0200

    Ajout du fichier src

---
+++ src/Lampe.java
@@ -0,0 +1,26 @@
+public class Lampe {
+    String nom;
+    boolean allume;
+
+    public Lampe(String nom){
+        this.nom = nom;
+        this.allume = false;
+    }
+
+    public void allumer(){
+        this.allume = true;
+    }
+
+    public void eteindre(){
+        this.allume = false;
+    }
+
+    public String toString(){
+        String msg = this.nom + " est ";
+        if (this.allume)
+            msg += "allumee";
+        else
+            msg += "eteinte";
+        return msg;
+    }
+}

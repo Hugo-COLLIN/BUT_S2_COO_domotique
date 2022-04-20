69d900bb2e44aa54c44ed2ea51fde98b93d4816f
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-04-20 16:11:51 +0200

    ajout liste hifi et structuration du code

--- Domotique/src/Telecommande.java
+++ Domotique/src/Telecommande.java
@@ -1,13 +1,33 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Telecommande {
+public class Telecommande
+{
+    //Attributes
     List<Lampe> lampes;
+    List<Hifi> hifi;
 
+    //Constructor
     public Telecommande(){
         this.lampes = new ArrayList<Lampe>();
+        this.hifi = new ArrayList<Hifi>();
     }
 
+    //Methods
+    // >General
+    public String toString(){
+        StringBuilder msg = new StringBuilder();
+        if (this.lampes.size() > 0) {
+            for (Lampe lampe : this.lampes) {
+                msg.append(lampe.toString()).append("\n");
+            }
+        } else
+            msg = new StringBuilder("La telecommande n'est reliee a aucune lampe");
+        return msg.toString();
+    }
+
+
+    // >Lampe
     public void ajouterLampe(Lampe lampe){
         this.lampes.add(lampe);
     }
@@ -20,20 +40,11 @@ public class Telecommande {
         this.lampes.get(indexLampe).eteindre();
     }
 
-    public void activerTout(){
+    public void activerToutLampes(){
         for(Lampe lampe : this.lampes){
             lampe.allumer();
         }
     }
 
-    public String toString(){
-        StringBuilder msg = new StringBuilder();
-        if (this.lampes.size() > 0) {
-            for (Lampe lampe : this.lampes) {
-                msg.append(lampe.toString()).append("\n");
-            }
-        } else
-            msg = new StringBuilder("La telecommande n'est reliee a aucune lampe");
-        return msg.toString();
-    }
+
 }

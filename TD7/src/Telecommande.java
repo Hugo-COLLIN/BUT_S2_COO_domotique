275377e9af4d88449bfe3e1d075d3b42ed02ed0a
DARDENNE Gregory <gdardenne0@gmail.com>
2022-04-27 15:52:52 +0200

    Copie coller des programmes du td6

---
+++ TD7/src/Telecommande.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Telecommande
+{
+    //Attributes
+    List<Lampe> lampes;
+    List<Hifi> hifis;
+
+    //Constructor
+    public Telecommande()
+    {
+        this.lampes = new ArrayList<Lampe>();
+    }
+
+    //Methods
+    public void ajouter(Lampe lampe){
+        this.lampes.add(lampe);
+    }
+
+    public void ajouter(Hifi hifi) { this.hifis.add(hifi);}
+
+    public void activerLampe(int indexLampe){
+        this.lampes.get(indexLampe).allumer();
+    }
+    public void activerHifi(int indexHifi){
+        this.hifis.get(indexHifi).allumer();
+    }
+
+    public void desactiverLampe(int indexLampe){
+        this.lampes.get(indexLampe).eteindre();
+    }
+
+    public void desactiverHifi(int indexHifi){
+        this.hifis.get(indexHifi).eteindre();
+    }
+    public void activerTout()
+    {
+        for(Lampe lampe : this.lampes)
+            lampe.allumer();
+        for(Hifi hifi : this.hifis)
+            hifi.allumer();
+    }
+
+    public String toString(){
+        StringBuilder msg = new StringBuilder();
+
+        if (this.lampes.size() > 0)
+        {
+            for (Lampe device : this.lampes)
+                msg.append(device.toString()).append("\n");
+        }
+        else
+            msg = new StringBuilder("La telecommande n'est reliee a aucune lampe.");
+
+        if (this.hifis.size() > 0)
+        {
+            for (Hifi device : this.hifis)
+                msg.append(device.toString()).append("\n");
+        }
+        else
+            msg = new StringBuilder("La telecommande n'est reliee a aucune chaine hifi.");
+
+        return msg.toString();
+    }
+}

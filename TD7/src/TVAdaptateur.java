e13bf57b5503770e9b284d3c1fc1759017330596
DARDENNE Gregory <gdardenne0@gmail.com>
2022-04-28 08:56:35 +0200

    ajout TVAdapteur

--- TD7/src/TVAdaptateur.java
+++ TD7/src/TVAdaptateur.java
@@ -1,2 +1,22 @@
 public class TVAdaptateur {
+    Television television;
+
+    public TVAdaptateur(){
+        this.television = new Television();
+    }
+
+    public void allumer(){
+        this.television.allumer();
+    }
+
+    public void eteindre(){
+        this.television.eteindre();
+    }
+
+    @Override
+    public String toString() {
+        return "TVAdaptateur{" +
+                "television=" + television +
+                '}';
+    }
 }

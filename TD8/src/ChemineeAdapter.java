7129325380289c5e2dfce11d76719a6f37fa24f8
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-04-28 09:53:33 +0200

    ajout Cheminee avec attribut et constructeur

---
+++ TD8/src/ChemineeAdapter.java
@@ -0,0 +1,14 @@
+public class ChemineeAdapter
+{
+    //Attribute
+    Cheminee c;
+
+
+    //Constructor
+    public ChemineeAdapter ()
+    {
+        this.c = new Cheminee();
+    }
+
+    //Methods
+}

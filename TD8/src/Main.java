91a0d8768e59251a2476e19993ab392ace6c7878
DARDENNE Gregory <gdardenne0@gmail.com>
2022-05-04 14:49:44 +0200

    Mise en forme du main

--- TD8/src/Main.java
+++ TD8/src/Main.java
@@ -4,20 +4,12 @@ public class Main {
 	public static void main(String args[])
 	{
 		Telecommande t=new Telecommande();
-		
-		
-		LampeAdaptateur l1=new LampeAdaptateur("Lampe1");
-		t.ajouterAppareil(l1);
+
+		t.ajouterAppareil(new LampeAdaptateur("Lampe1"));
 		t.ajouterAppareil(new HifiAdaptateur());
 		t.ajouterAppareil(new ChemineeAdapter());
-		
-		/****** A COMPLETER ******/
-		// TODO
-		
-		TelecommandeGraphique tg=new TelecommandeGraphique(t);
-		
 
-		
+		TelecommandeGraphique tg=new TelecommandeGraphique(t);
 	}
 	
 }
